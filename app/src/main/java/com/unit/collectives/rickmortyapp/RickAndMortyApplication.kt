c7a76748d25697a0b3af1b83d8dc0e9af56e37bd
UrielOM <urielom@kata-software.com>
2022-05-06 13:34:43 -0500

    hilt advance

---
+++ app/src/main/java/com/unit/collectives/rickmortyapp/RickAndMortyApplication.kt
@@ -0,0 +1,10 @@
+package com.unit.collectives.rickmortyapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RickAndMortyApplication: Application() {
+
+
+}
